9df4009fb9af5dcd1a96e74697f10854f1306291
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2024-10-07 17:33:01 +0700

    Creating Service and  use eventDao inside Impl

--- src/main/java/se331/lab_new/service/EventService.java
+++ src/main/java/se331/lab_new/service/EventService.java
@@ -1,4 +1,11 @@
 package se331.lab_new.service;
 
+import se331.lab_new.entity.Event;
+
+import java.util.List;
+
 public interface EventService {
+    Integer getEventSize();
+    List<Event> getEvents(Integer pageSize, Integer page);
+    Event getEvent(Long id);
 }
--- src/main/java/se331/lab_new/service/EventServiceImpl.java
+++ src/main/java/se331/lab_new/service/EventServiceImpl.java
@@ -1,4 +1,32 @@
 package se331.lab_new.service;
 
-public class EventServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import se331.lab_new.dao.EventDao;
+import se331.lab_new.entity.Event;
+
+import java.util.List;
+
+//@Service annotation is used with classes that provide some business functionalities.
+
+@Service
+@RequiredArgsConstructor
+
+public class EventServiceImpl implements EventService {
+    final EventDao eventDao;
+
+    @Override
+    public Integer getEventSize() {
+        return eventDao.getEventSize();
+    }
+
+    @Override
+    public List<Event> getEvents(Integer pageSize, Integer page) {
+        return eventDao.getEvents(pageSize, page);
+    }
+
+    @Override
+    public Event getEvent(Long id) {
+        return eventDao.getEvent(id);
+    }
 }
